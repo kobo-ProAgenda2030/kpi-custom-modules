497e295fb19827131fde3d531cd689b5ecdec945
Samuel Rioja <samuelriotz@hotmail.com>
2021-03-12 11:06:05 -0400

    adding user roles

--- src/models/UserRole.ts
+++ src/models/UserRole.ts
@@ -1,10 +1,4 @@
 export interface UserRole {
-  id: string;
+  roleId: string;
   name: string;
 }
-export const userRoles: UserRole[] = [
-  { id: "1", name: "name 1" },
-  { id: "2", name: "name 2" },
-  { id: "3", name: "name 3" },
-  { id: "4", name: "name 4" },
-];
