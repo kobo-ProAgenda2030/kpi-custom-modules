3f7e74d79aece5de223ab5c629c5197800ffa0d1
Samuel Rioja <samuelriotz@hotmail.com>
2021-03-12 10:23:07 -0400

    adding services

--- src/service/serverConnection.ts
+++ src/service/serverConnection.ts
@@ -1,23 +1,30 @@
 import axios, { AxiosInstance } from "axios";
-import { KoboUser } from "../models/KoboUser";
-import { Organization } from "../models/Organization";
 
 export class ServerConnection {
-  private instance: AxiosInstance = axios.create();
-  load(baseUR: string) {
+  private instance: AxiosInstance;
+  constructor(baseURL: string) {
+    const token = localStorage.getItem("sessionToken");
     this.instance = axios.create({
-      baseURL: baseUR,
+      baseURL: baseURL,
       headers: {
         "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJzdXBlcl9hZG1pbiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdXNlcmRhdGEiOiIwNGNhOTczMGU0NTlmZjYyNTg4OTgyZmM5M2EwZTdlMTFkNjVkNzc0IiwibmJmIjoxNjE1MzE5MzQ5LCJleHAiOjE2MTc5MTEzNDksImlhdCI6MTYxNTMxOTM0OX0.znF2vGifK5HWADN0FNIBj_M5v0TZARpThKK6jCQY8vJhFWczmrCtlIE-_lgd5gmeAZSf0LzoqZy3-L5cITFsNA",
       },
     });
   }
-  async getKoboUsers(): Promise<KoboUser[]> {
-    const response = await this.instance.get("/kobo-users");
+  async get<T>(url: string): Promise<T> {
+    const response = await this.instance.get(url);
+    await sleep(2000);
     return response.data;
   }
-  async getOrganizations(): Promise<Organization[]> {
-    const response = await this.instance.get("/organizations");
+  async post<T>(url: string, body: any): Promise<T> {
+    const response = await this.instance.post(url, JSON.stringify(body));
+    await sleep(2000);
     return response.data;
   }
+}
+export const serverConnection = new ServerConnection("http://localhost:63253");
+function sleep(millis: number) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
 }
---
+++ src/service/services.ts
@@ -0,0 +1,59 @@
+import { KoboUser, KoboUserPost, KoboUserResource } from "../models/KoboUser";
+import { Organization, OrganizationPost } from "../models/Organization";
+import { PostProfile, Profile } from "../models/Profile";
+import { UserRole } from "../models/UserRole";
+import { ServerConnection } from "./serverConnection";
+
+export class Services {
+  private serverConnection = new ServerConnection("");
+  load(baseURL: string) {
+    this.serverConnection = new ServerConnection(baseURL);
+  }
+  //User management
+  async getAllKoboUsers(): Promise<KoboUser[]> {
+    return this.serverConnection.get<KoboUser[]>("/KoboUser");
+  }
+  async getKoboUserResources(): Promise<KoboUserResource> {
+    return this.serverConnection.get<KoboUserResource>(
+      "/koboUser/userResources"
+    );
+  }
+  async updateKoboUser(body: KoboUserPost): Promise<void> {
+    await this.serverConnection.post("/koboUser", body);
+  }
+  //Organization management
+  async getAllOrganizations(): Promise<Organization[]> {
+    return this.serverConnection.get<Organization[]>("/Organization/All");
+  }
+  async updateCreateOrganization(
+    body: OrganizationPost
+  ): Promise<{ organizationId: string }> {
+    return this.serverConnection.post<{ organizationId: string }>(
+      "/Organization/Update",
+      body
+    );
+  }
+  async deleteOrganization(id: string): Promise<void> {
+    await this.serverConnection.get<KoboUser[]>(
+      `/Organization/Delete?organizationId=${id}`
+    );
+  }
+  //role management
+  async getAllRoles(): Promise<UserRole[]> {
+    return this.serverConnection.get<UserRole[]>("/Role/All");
+  }
+  //Profile management
+  async getAllProfiles(): Promise<Profile[]> {
+    return this.serverConnection.get<Profile[]>("/Profile/All");
+  }
+  async updateCreateProfile(body: PostProfile): Promise<{ profileId: string }> {
+    return this.serverConnection.post<{ profileId: string }>(
+      "/Profile/Update",
+      body
+    );
+  }
+  async getProfile(id: string): Promise<Profile> {
+    return this.serverConnection.get<Profile>(`/Profile/Get?profileId=${id}`);
+  }
+}
+export const services = new Services();
